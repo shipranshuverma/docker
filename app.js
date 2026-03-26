13b10328d7c8de8885faa759960dfa669cb072db
Shipranshu Verma <ssshipranshuverma@gmail.com>
2026-03-26 13:44:27 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "mysql",   // IMPORTANT (service name)
+  user: "root",
+  password: "password",
+  database: "testdb"
+});
+
+db.connect(err => {
+  if (err) {
+    console.log("DB connection failed:", err);
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Node connected to MySQL 🚀");
+});
+
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, result) => {
+    if (err) return res.send(err);
+    res.json(result);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
